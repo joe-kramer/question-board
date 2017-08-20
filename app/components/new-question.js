717d865def27f61efa732565ee60e78891cee146
Joe Kramer <joekramer1125@gmail.com>
2017-08-20 15:59:08 -0700

    change attr question to questionAsked to prevent naming issues

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -10,7 +10,7 @@ export default Ember.Component.extend({
     saveQuestion() {
       var params = {
         author: this.get('author'),
-        question: this.get('question'),
+        questionAsked: this.get('questionAsked'),
         notes: this.get('notes')
       };
       this.set('addNewQuestion', false);
--- app/components/update-question.js
+++ app/components/update-question.js
@@ -9,7 +9,7 @@ export default Ember.Component.extend({
     update(question) {
       var params = {
         author: this.get('author'),
-        question: this.get('question'),
+        questionAsked: this.get('questionAsked'),
         notes: this.get('notes')
       };
       this.set('updateQuestionForm', false);
